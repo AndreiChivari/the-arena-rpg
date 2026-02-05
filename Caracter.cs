8429e4bfeffe36b458f13d1bdec7f0f9336d2874
chivari.andrei <andrei.chivari@gmail.com>
2026-02-05 21:34:25 +0200

    Added static and params constructors

--- Caracter.cs
+++ Caracter.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace TheArenaRPG
 {
-    internal class Caracter
+    public class Caracter
     {
+        public string Nume { get; set; }
+        public int Viata { get; private set; }
+        public int Putere { get; set; }
+
+        public static int NumarTotalCaractere;
+
+        static Caracter()
+        {
+            NumarTotalCaractere = 0;
+            Console.WriteLine("[GAME]: Motorul jocului a initializat clasa Caracter");
+        }
+
+        //Constructor
+        public Caracter(string nume, int viata, int putere)
+        {
+            Nume = nume;
+            Viata = viata;
+            Putere = putere;
+
+            NumarTotalCaractere++;
+        }
+
+        public Caracter(string nume) : this(nume, 100, 10)
+        {
+        }
+
+        public Caracter(Caracter original) : this(original.Nume, original.Viata, original.Putere)
+        {
+            Console.WriteLine($"[CLONARE]: S-a creat o copie a lui {original.Nume}");
+        }
+
+        // Metode
+
+        public void AfiseazaStatut()
+        {
+            Console.WriteLine($"Fisa {Nume}: HP {Viata} | STR {Putere}");
+        }
+
+        public void IncaseazaLovitura(int damage)
+        {
+            if (damage < 0) return;
+            Viata -= damage;
+
+            Console.WriteLine($"{Nume} a fost lovit! A pierdut {damage} HP. Viata ramasa: {Viata}");
+        
+            if(Viata <= 0)
+            {
+                Viata = 0;
+                Console.WriteLine($"{Nume} a lesinat!");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace TheArenaRPG
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Jocul a pornit cu {Caracter.NumarTotalCaractere} jucatori!");
+
+            Caracter erou = new Caracter("Hercule");
+            erou.AfiseazaStatut();
+
+            Caracter inamic = new Caracter("Minotaur", 500, 50);
+            inamic.AfiseazaStatut();
+
+            Caracter copieInamic = new Caracter(inamic);
+            copieInamic.Nume = "Minotaur Jr.";
+            copieInamic.AfiseazaStatut();
+
+            Console.WriteLine($"Avem {Caracter.NumarTotalCaractere} jucatori!");
+
+            Console.ReadKey();
         }
     }
 }
